93b0342860dd86e69ba055927d1337aa45842bc1
ppesq <betoesquivel>
2013-10-28 02:41:43 -0600

    Finished problem, haven't checked it with the judge.

---
+++ bst.cpp
@@ -0,0 +1,111 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  bst.cpp
+ *
+ *    Description:  Input is number of cases. One line with the order of the matrix.
+ *    BST contains at most 100 elements. A line in which contains the value of -1 
+ *    indicates the end of input.
+ *
+ *    Output, for each test case, print the case number, followed by messages
+ *    indicating the height of the BST and the average of comparisons to search
+ *    the keys of the BST in the form.
+ *
+ *        Version:  1.0
+ *        Created:  28/10/13 00:47:32
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  YOUR NAME (), 
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  calcularComparacionesPromedio
+ *  Description:  
+ * =====================================================================================
+ */
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+const int maxRows = 100, maxColumns = 100;
+int matrix[maxRows][maxColumns];
+int rows, columns;
+
+int leftNode(int rootRow, int rootColumn)
+{
+	rootColumn--;
+	while(matrix[rootRow][rootColumn]==matrix[rootRow][rootColumn+1])
+	{
+		rootColumn--;
+	}
+	return rootColumn;
+}
+int rightNode(int rootRow, int rootColumn)
+{
+	rootRow++;
+	while(matrix[rootRow][rootColumn]==matrix[rootRow-1][rootColumn])
+	{
+		rootRow++;
+	}
+	return rootRow;
+}
+int calculateTotalNodes ( int rootRow, int rootColumn )
+{
+	if (matrix[rootRow][rootColumn]!=0) {
+		return  (
+				1
+				+ calculateTotalNodes ( rootRow, leftNode(rootRow,rootColumn) )
+				+ calculateTotalNodes ( rightNode(rootRow,rootColumn), rootColumn )
+			);
+	}else{
+		return 0;
+	}
+
+}		
+double calculateAverageComparisons ( int rootRow,  int rootColumn, int totalNodes, int comparisons )
+{
+	if (matrix[rootRow][rootColumn]!=0) {
+		return  (
+				comparisons*(1.0/totalNodes) 
+				+ calculateAverageComparisons(rootRow,leftNode(rootRow,rootColumn), totalNodes, (comparisons+1)) 
+				+ calculateAverageComparisons(rightNode(rootRow,rootColumn),rootColumn,totalNodes, (comparisons+1))
+			);
+	}else{
+		return 0.0;
+	}
+
+}		
+int main(int argc, const char *argv[])
+{
+	int matrixOrder;
+	cin>>matrixOrder;
+	rows = columns = matrixOrder;
+
+	while(matrixOrder!=-1)
+	{
+		for (int r = 0; r < rows; r++) {
+			for (int c = 0; c < columns; c++) {
+				cin>>matrix[r][c];
+			}
+		}
+		
+		int totalNodes = calculateTotalNodes( 0, columns-1 );
+		cout<<totalNodes<<endl;
+		int height = matrixOrder/2 - 1;
+		double averageComparisons = calculateAverageComparisons( 0, columns-1, totalNodes, 1 );
+		
+		cout<<"The height of the BST is: "<<height<<endl;
+		cout<<setprecision(2);
+		cout<<"The average of the search in the BST is: "<<fixed<<averageComparisons<<endl;
+
+		cin>>matrixOrder;
+		rows = columns = matrixOrder;
+	}	
+	return 0;
+}
